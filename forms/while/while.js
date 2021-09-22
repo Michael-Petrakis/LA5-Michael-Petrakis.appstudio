4b8280a52c39e4464ab81c28a9e00fd38a67c119
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-09-22 16:10:54 -0500

    While Iteration Program

---
+++ forms/while/while.js
@@ -0,0 +1,7 @@
+let wantToContinue = 'true'
+let i = 1
+while(wantToContinue) {
+  console.log(`This is loop iteration ${i}`)
+  wantToContinue = confirm("Do you want to go again? Okay for yes, Cancel for No")
+  i++
+  }
