c0ed199bfbce26fc3d9e49c46d9649ae22c6c46b
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-09-25 12:22:16 -0500

    Version 1

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -1,3 +1,4 @@
+/*
 function calculate(n1, n2) {
   let answerFunction = n1+n2
   return answerFunction
@@ -10,3 +11,4 @@ let number2 = Number(prompt("Enter an integer"))
 let summedNumbers = calculate(number1, number2)
 
 console.log(`The sum of ${number1} and ${number2} is ${summedNumbers}`)
+*/
